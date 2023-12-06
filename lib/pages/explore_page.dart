219f226d68e0fc386898272d548bcd0c4397a438
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-12-06 07:18:23 +0530

    Added - consultation card

--- lib/pages/explore_page.dart
+++ lib/pages/explore_page.dart
@@ -9,32 +9,66 @@ class ExplorePage extends StatelessWidget {
     return Scaffold(
         body: ListView(padding: EdgeInsets.all(16), children: [
       // filter and search text field
-      Row(children: [
-        const Expanded(
-            child: TextField(
-          decoration: InputDecoration(
-              contentPadding: EdgeInsets.all(12),
-              prefixIcon: Icon(IconlyLight.search),
-              hintText: 'Search here...',
-              isDense: true,
-              border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey,
-                  ),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(99),
-                  ))),
-        )),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0),
-          child: IconButton(
-              onPressed: () {},
-              icon: IconButton.filledTonal(
-                  onPressed: () => {}, icon: const Icon(IconlyLight.filter))),
-        )
-      ])
+      Padding(
+        padding: const EdgeInsets.only(bottom: 12),
+        child: Row(children: [
+          const Expanded(
+              child: TextField(
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(12),
+                prefixIcon: Icon(IconlyLight.search),
+                hintText: 'Search here...',
+                isDense: true,
+                border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey,
+                    ),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(99),
+                    ))),
+          )),
+          Padding(
+            padding: const EdgeInsets.only(left: 8.0),
+            child: IconButton(
+                onPressed: () {},
+                icon: IconButton.filledTonal(
+                    onPressed: () => {}, icon: const Icon(IconlyLight.filter))),
+          )
+        ]),
+      ),
 
       // consultation card
+      SizedBox(
+        height: 170,
+        child: Card(
+            color: Colors.green.shade50,
+            elevation: 0.1,
+            shadowColor: Colors.green.shade50,
+            child: Padding(
+              padding: const EdgeInsets.all(12),
+              child: Row(children: [
+                Flexible(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text('Free Consultation',
+                          style: Theme.of(context)
+                              .textTheme
+                              .titleLarge!
+                              .copyWith(color: Colors.green.shade700)),
+                      Text('Get free support from our customer service',
+                          style: Theme.of(context).textTheme.bodyMedium),
+                      FilledButton(
+                          onPressed: () => {}, child: const Text("Call now"))
+                    ],
+                  ),
+                ),
+                // image
+                Image.asset('assets/contact_us.png')
+              ]),
+            )),
+      )
     ]));
   }
 }
