043d1114b030a8b50a6ae389963b0d5e4fdda97e
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-12-11 02:23:06 +0530

    Added - Service page

---
+++ lib/models/service.dart
@@ -0,0 +1,6 @@
+class Service {
+  final String name;
+  final String image;
+
+  const Service({required this.name, required this.image});
+}
--- lib/pages/services.page.dart
+++ lib/pages/services.page.dart
@@ -1,10 +1,49 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+// services
+import 'package:agri_plant/data/services.dart';
 
 class ServicesPage extends StatelessWidget {
   const ServicesPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        body: GridView.builder(
+      padding: EdgeInsets.all(16),
+      itemCount: services.length,
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          crossAxisSpacing: 14,
+          mainAxisSpacing: 14,
+          childAspectRatio: 0.85),
+      itemBuilder: (context, index) {
+        final service = services[index];
+        return Container(
+            alignment: Alignment.bottomCenter,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                    fit: BoxFit.cover, image: AssetImage(service.image))),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(10),
+              child: BackdropFilter(
+                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+                child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
+                    decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.2),
+                        borderRadius: BorderRadius.circular(10)),
+                    child: Text(service.name,
+                        style: const TextStyle(
+                            color: Colors.white, fontSize: 16))),
+              ),
+            ));
+      },
+    ));
   }
 }
